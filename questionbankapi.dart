75f9af7779e4c25aed0b6ac2fba6dfbf25016fce
Kamal Vaishnav <76464850+Kamalkvaishnav@users.noreply.github.com>
2022-06-20 16:00:33 +0530

    Create questionbankapi.dart

---
+++ questionbankapi.dart
@@ -0,0 +1,52 @@
+
+{ quizquestions :
+    [ { category: 'science', 'sub-category0': 'physics', 'sub-category1': 'light', levels: 'easy'
+      , question: 'Which color is not part of VIBGYOR', answer: 'Brown'
+      , option1: 'Violet', option2: 'Red', option3: 'Orange', option4: 'Brown'
+      } 
+    , { category: 'science', 'sub-category0': 'physics', 'sub-category1': 'energy', levels: 'moderate'
+      , question: 'Which form of energy is renewable form of energy?', answer: 'solar'
+      , option1: 'electric', option2: 'solar', option3: 'coal', option4: 'petroleum'
+      } 
+    , { category: 'science', 'sub-category0': 'chemistry', 'sub-category1': 'Elements', levels: 'hard'
+      , question: 'Gold is represented in the periodic table by which Symbol?', answer: 'Au'
+      , option1: 'Go', option2: 'Au', option3: 'Ye', option4: 'ld'
+      } 
+    , { category: 'science', 'sub-category0': 'chemistry', 'sub-category1': 'Elements', levels: 'moderate'
+      , question: 'Tin is represented in the periodic table by which Symbol?', answer: 'Sb'
+      , option1: 'Go', option2: 'Au', option3: 'Sb', option4: 'ld'
+      } 
+    , { category: 'science', 'sub-category0': 'chemistry', 'sub-category1': 'Elements', levels: 'easy'
+      , question: 'Lead is represented in the periodic table by which Symbol?', answer: 'Pb'
+      , option1: 'Pb', option2: 'Au', option3: 'Sb', option4: 'ld'
+      } 
+    , { category: 'social science', 'sub-category0': 'history', 'sub-category1': 'worldwar', levels: 'hard'
+      , question: 'Where were the atom bombs dropped?', answer: 'Japan'
+      , option1: 'Germany', option2: 'Poland', option3: 'Russia', option4: 'Japan'
+      } 
+    , { category: 'social science', 'sub-category0': 'history', 'sub-category1': 'USA', levels: 'hard'
+      , question: 'When did USA become independent?', answer: '1776'
+      , option1: '1774', option2: '1776', option3: '1876', option4: '1879'
+      } 
+    , { category: 'social science', 'sub-category0': 'history', 'sub-category1': 'USA', levels: 'hard'
+      , question: 'Who was the first presidnet of USA?', answer: 'George Washington'
+      , option1: 'John Adams', option2: 'Thomas Jefferson', option3: 'George Washington', option4: 'James Madison'
+      } 
+    , { category: 'social science', 'sub-category0': 'history', 'sub-category1': 'USA', levels: 'hard'
+      , question: 'What is the capital of USA?', answer: 'Washington'
+      , option1: 'New York', option2: 'Washington', option3: 'Chicago', option4: 'California'
+      } 
+    , { category: 'general knowledge', 'sub-category0': 'capital', 'sub-category1': 'Europe', levels: 'easy'
+      , question: 'What is the capital of France?', answer: 'Paris'
+      , option1: 'Paris', option2: 'London', option3: 'Brussels', option4: 'Moscow'
+      } 
+    , { category: 'general knowledge', 'sub-category0': 'capital', 'sub-category1': 'Europe', levels: 'easy'
+      , question: 'What is the capital of Russia?', answer: 'Moscow'
+      , option1: 'Paris', option2: 'London', option3: 'Brussels', option4: 'Moscow'
+      }
+    , { category: 'general knowledge', 'sub-category0': 'capital', 'sub-category1': 'Europe', levels: 'easy'
+      , question: 'What is the capital of Portugal?', answer: 'Lisbon'
+      , option1: 'Paris', option2: 'London', option3: 'Lisbon', option4: 'Moscow'
+      }
+    ]
+  }
